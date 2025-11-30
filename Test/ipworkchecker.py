7ac203d0e0f791dbc09ca5e19e19aef2d491b75a
Rishi0022 <rishi.im22@gmail.com>
2025-11-30 10:32:24 -0800

    first commit

---
+++ Test/ipworkchecker.py
@@ -0,0 +1,19 @@
+import requests
+
+proxy = "40.192.16.115:8085"
+
+proxies = {
+    "http": f"http://{proxy}",
+    "https": f"http://{proxy}"
+}
+
+try:
+    response = requests.get("http://httpbin.org/ip", proxies=proxies, timeout=5)
+    print("Proxy is working")
+    print("Response:", response.json())
+except Exception as e:
+    print("Proxy failed")
+    print("Error:", e)
+
+#no clue gang
+
