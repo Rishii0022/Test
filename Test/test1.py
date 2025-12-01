19fc8c812f0f05d97c6b7efe8152843e3a333a56
Rishi0022 <rishi.im22@gmail.com>
2025-11-30 21:10:30 -0800

    main copy

--- Test/test1.py
+++
@@ -1,5 +0,0 @@
-from swiftshadow.classes import ProxyInterface
-
-# Fetch HTTPS proxies from the US
-swift = ProxyInterface(countries=["IN"], protocol="https")
-print(swift.get().as_string())  # Output: https://<ip>:<port>
